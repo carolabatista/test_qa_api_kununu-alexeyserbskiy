4e47001488fcb5911edaa49e60437c513fcbec91
licontrop <amserbskiy@gmail.com>
2019-04-11 15:09:28 +0200

    Adding __init__.py file
    Adding __init__.py file is making a folder a package, so classes, methods from different files can be imported. Also, this file contains "teardown_module", which will be run only once for the hole package of tests.
    
    I have put printing placeholder in "teardown_module" in order to show, that, in theory, inside the tests code, some cleaning operations may take place, if test data is not cleaned by Jenkins or other CI tools pr scripts.

---
+++ __init__.py
@@ -0,0 +1,3 @@
+
+def teardown_module(module):
+    print("Here must be some code, that will initialise test DB be cleaned from created test data. \nJust in case if it is not implemented in the pipeline.")
