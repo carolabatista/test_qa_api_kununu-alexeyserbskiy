207e411f2169ab19df692d855a7ddb5b9c782d0d
licontrop <amserbskiy@gmail.com>
2019-04-11 16:41:51 +0200

    Adding New Tests to registration suite
    New user registration test uses now unique email. Registration test suite has test for checking uniqueness of generated tokens.
    
    Using unique email in registration test is needed to fulfill the requirements of registration data. If email will not be unique, test will fail, because server must respond an error.

--- BaseSettings.py
+++ BaseSettings.py
@@ -5,25 +5,31 @@ import requests
 
 class BaseSettings(TestCase):
 
+    #Amount of users, created during a tests run
+    users = 0
+
     # Used for every test class
     def setup_class(self):
-        print("----test started----")
+        print("----Test suite started----")
 
     def teardown_class(self):
-        print("----test is over----")
+        print("----Test suite is over----")
 
     # Used for every test method in class
     def setUp(self):
-        print('----current test: ' + self._testMethodName + " started----")
+        print("----Current testcase: " + self._testMethodName + " started----")
 
     def tearDown(self):
-        print('----current test: ' + self._testMethodName + " is over----")
+        print("----Current testcase: " + self._testMethodName + " is over----")
 
     # Function, that will store JSONs
-    def get_JSON(self, endpoint, filename):
+    def write_JSON(self, endpoint, filename):
         jsondata = requests.get(endpoint).json()
-        with open(filename + '.txt', 'w') as json_file:
+        with open(filename + ".txt", "w") as json_file:
             json.dump(jsondata, json_file)
+
+
+
 
 
 
--- test_Registration.py
+++ test_Registration.py
@@ -3,12 +3,34 @@ import requests
 
 class TestRegistration (BaseSettings):
 
-    def test_Register_new_user (self):
-        r = requests.post("https://reqres.in/api/register", json={"email": "test@test.test", "password": "p"})
-        assert (r.status_code == 201), "Response code is not 201"
-        print(r.json())
-        assert (isinstance(r.json()["token"], str)), "Token is not string"
-        assert (r.json()["token"] != ""), "Token is empty"
+    #storing token is needed for testing its uniqueness
+    token: str
+
+    # Testcase for positive straight scenario of registration
+    def test_Register_New_User(self):
+        users = BaseSettings.users + 1
+        BaseSettings.users = users
+        registration = requests.post("https://reqres.in/api/register", json={"email": "test@test{}.test".format(users), "password": "Password1"})
+        assert (registration.status_code == 201), "Response code is {}, instead of 201".format(registration.status_code)
+        assert (isinstance(registration.json()["token"], str)), "Token is not a string"
+        assert (registration.json()["token"] != ""), "Token is empty"
+
+        # storing token is needed for further testing of its uniqueness
+        TestRegistration.token = registration.json()["token"]
+
+
+    # Testcase for checking uniqueness of generated tokens
+    def test_Registration_Token_is_Unique(self):
+        TestRegistration.test_Register_New_User(self)
+
+        # "current" string is added to imitate that server always generates a new token (what it does not), so the test will not fail.
+        current_token = TestRegistration.token + "current"
+
+        TestRegistration.test_Register_New_User(self)
+        assert (current_token != TestRegistration.token)
+
+
+
 
 
 
